f259fc9d356e1dca5c0d91fbce10f34f21edd182
“yunus-emre <yunusemreozkilic@outlook.com>
2024-05-14 19:05:02 +0200

    Facebook Integration

--- custom_addons/chatgpt_api/models/models.py
+++ custom_addons/chatgpt_api/models/models.py
@@ -4,6 +4,7 @@ import os
 from openai import OpenAI
 import requests
 from odoo import models, fields, api
+import facebook
 
 
 class chatgpt_api(models.Model):
@@ -17,6 +18,8 @@ class chatgpt_api(models.Model):
     def send_prompt(self):
         self.response = self._get_chatgpt_response(self.prompt)
 
+
+            
     def _get_chatgpt_response(self, prompt):
         try:
             client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
@@ -31,3 +34,25 @@ class chatgpt_api(models.Model):
             return completion.choices[0].message.content
         except Exception as e:
             return f"Error: {str(e)}"
+
+    
+
+    
+    @api.depends('get_facebook_profile')
+    def get_facebook_profile(self):
+        access_token = os.getenv('FACEBOOK_ACCESS_TOKEN')
+        success, profile = self._get_facebook_profile(access_token)
+        if success:
+            print("Facebook profil bilgileri:", profile)
+            self.response = profile['name']
+        else:
+            print("Facebook profil bilgileri alınamadı. Hata:", profile)
+
+        
+    def _get_facebook_profile(self, access_token):
+        graph = facebook.GraphAPI(access_token)
+        try:
+            profile = graph.get_object("me", fields="id,name,email")
+            return True, profile
+        except facebook.GraphAPIError as e:
+            return False, "Failed to retrieve Facebook profile. Error: {}".format(e.message)
